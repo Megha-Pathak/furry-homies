f06dda535d634d7827b36c0505beafb29dc19320
Megha <meghapathak2013@gmail.com>
2020-09-02 13:29:00 +0530

    custom hooks done

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   const [location, updateLocation] = useState("Seattle, WA");
-  const [animal, updateAnimal] = useState("dog");
-  
-
+  const [breeds, updateBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown, updateBreed] = useDropdown("Breed", "", breeds);
   return (
     <div className="search-params">
+       
       <form>
         <label htmlFor="location">
           Location
@@ -15,23 +17,11 @@ const SearchParams = () => {
             id="location"
             value={location}
             placeholder="Location"
-            onChange = {e => updateLocation(e.target.value)}
+            onChange={e => updateLocation(e.target.value)}
           />
-          <label>
-              Animals: 
-            <select
-            id = "animal"
-            value = {animal}
-            onChange = {e => (e.target.value)}
-            onBlur = {e => (e.target.value)}>
-                <option>All</option>
-               {/* Now we will map array of strings by the Pet API to Array of Option Component */}
-                {ANIMALS.map(animal => <option value = {animal}>{animal}</option>)}
-            </select>
-          </label>
-
         </label>
-      
+        <AnimalDropdown />
+        <BreedDropdown />
         <button>Submit</button>
       </form>
     </div>
