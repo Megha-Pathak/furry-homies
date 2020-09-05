41f1cb6df5423da82e6a4a5c72622774df7641c0
Megha <meghapathak2013@gmail.com>
2020-09-05 17:06:49 +0530

    added carousel.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
 import Search from "./SearchParams"
+import { Router, Link } from "@reach/router";
+import Details from "./Details";
 
 function App() {
   return (
     <div>
-    <h1>Furry Homies</h1>
-    <Search/>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
+      <Router>
+        <Search path="/" />
+        <Details path="/details/:id" />
+      </Router>
   </div>
   );
 }
